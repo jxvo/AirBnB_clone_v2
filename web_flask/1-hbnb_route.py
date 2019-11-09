7b29bdadff1e5b28c0fc779d74a8a00e10a6db35
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-09 01:50:03 +0000

    start a Flask web app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""starts a flask web app"""
+
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route('/', strict_slashes=False)
+def hello_world():
+    """prints hello_world"""
+    return "Hello HBNB!"
+
+
+@application.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints hbnb"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=5000)
